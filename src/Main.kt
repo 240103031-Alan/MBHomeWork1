5a7a1861eced4fcdf24c9de73290ab092b838843
AlanAgynuly <alan.agynuly@gmail.com>
2026-09-25 10:39:09 +0500

    HM1: Task1

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,6 @@
 fun main() {
-    var discountPercentage: Int = 0
-    var offer: String = ""
-    val item = "Google Chromecast"
-
-    discountPercentage = 20
-    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
-
-    println(offer)
+    println("Use the val keyword when the value doesn't change.")
+    println("Use the var keyword when the value can change.")
+    println("When you define a function, you define the parameters that can be passed to it.")
+    println("When you call a function, you pass arguments for the parameters.")
 }
